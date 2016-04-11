6d4a546d357862e462ef305ba6292172a8631720
alexandrastc <alexandrastc09@gmail.com>
2016-04-11 16:38:50 +0300

    Updated main

--- main.c
+++ main.c
@@ -25,15 +25,7 @@ int main()
 
 	//"menu" displaying user's options
 
-	printf("------------------ TO-DO LIST -----------------\n");
-	printf("\n\n\t1.Add new task");
-	printf("\n\t2.Display all tasks");
-	printf("\n\t3.Display by category");
-	printf("\n\t4.Sort by priority");
-	printf("\n\t5.Delete task");
-	printf("\n\t6.Quit\n\n\n");
-
-	printf("Your choice : ");
+	menu();
 	
 	int choice = getchar();
 
