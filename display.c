f1cb50ef5d51d440bc7b67611fc41d7f3936f493
alexandrastc <alexandrastc09@gmail.com>
2016-04-11 16:30:57 +0300

    Source code for the display header file

---
+++ display.c
@@ -0,0 +1,17 @@
+#include "display.h"
+#include <stdio.h>
+
+void menu()
+{
+
+	printf("------------------ TO-DO LIST -----------------\n");
+	printf("\n\n\t1.Add new task");
+	printf("\n\t2.Display all tasks");
+	printf("\n\t3.Display by category");
+	printf("\n\t4.Sort by priority");
+	printf("\n\t5.Delete task");
+	printf("\n\t6.Quit\n\n\n");
+
+	printf("Your choice : ");
+
+}
