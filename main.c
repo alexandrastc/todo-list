e9453006306a9571f194750f07ccc780daf27bb4
alexandrastc <alexandrastc09@gmail.com>
2016-04-11 18:12:09 +0300

    Updated main to use the new functions

--- main.c
+++ main.c
@@ -4,14 +4,6 @@
 #include "list.h"
 #include "display.h"
 
-typedef struct NOD{
-	
-	int priority;
-	char* category;
-	char* task; 
-	struct NOD *next;
-
-} NODE;
 
 int main()
 {
@@ -23,10 +15,41 @@ int main()
 
 	head = (NODE*) malloc(sizeof(NODE));
 
-	//"menu" displaying user's options
+	//"menu" displaying user's options, takes no input
 
 	menu();
 	
 	int choice = getchar();
+
+	while (choice != '6'){
+	
+		switch (choice){
+			case '1':add_task(head,1,"Wubba","Lubba Dub Dub");
+					 printf("Task added.\n");
+				break;
+			case '2':print_all(head);
+				break;
+			case '3':;
+				break;
+			case '4':;
+				break;
+			case '5':;
+				break;
+			case '6': break;
+				break;
+			default: 
+				printf("\nPlease enter your choice (1 to 6).\n");
+		}
+		
+		choice = getchar();
+		
+		if (choice != '6'){
+		
+			menu();
+		
+		}
+	}
+
+	return 0;
 
 }
