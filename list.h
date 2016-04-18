bb105f4aada285f8e26be652663df2e73708a9c6
alexandrastc <alexandrastc09@gmail.com>
2016-04-18 17:21:44 +0300

    Added a function to display by category

--- list.h
+++ list.h
@@ -1,17 +1,6 @@
-#ifndef _LIST_H_
-#define _LIST_H_ 
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_ 
 
-typedef struct NOD{
-	
-	int priority;
-	char* category;
-	char* task; 
-	struct NOD *next;
-
-} NODE;
-
-	void add_task(NODE *head, int p, char *c, char *t);
-	void print_all(NODE *head);	
-
+	void menu();
 
 #endif
