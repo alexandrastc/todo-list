7804455a61a3bef30c73f13d235a4c057cadd6dd
alexandrastc <alexandrastc09@gmail.com>
2016-05-09 17:39:46 +0300

    Updated main to use the delete task function

--- main.c
+++ main.c
@@ -4,14 +4,6 @@
 #include "list.h"
 #include "display.h"
 
-typedef struct NOD{
-	
-	int priority;
-	char* category;
-	char* task; 
-	struct NOD *next;
-
-} NODE;
 
 int main()
 {
@@ -23,10 +15,63 @@ int main()
 
 	head = (NODE*) malloc(sizeof(NODE));
 
-	//"menu" displaying user's options
+	//"menu" displaying user's options, takes no input
 
 	menu();
 	
 	int choice = getchar();
+
+	while (choice != '8' ){
+	
+		switch (choice){
+			
+			case '1':
+					 add_task(head,1,"wubba","lubba dub dub");
+					 add_task(head,2,"i need","a break");
+					 add_task(head,3,":(","ughhh");
+					 printf("Task added.\n");
+				break;
+			
+			case '2':
+					 print_all(head);
+				break;
+			
+			case '3':
+					 print_by_categ(head,"wubba");
+				break;
+			
+			case '4':
+					MergeSort(&head);
+					printf("All sorted.\n");
+				break;
+			
+			case '5':
+					delete_task(head,"a break");
+					printf("Task deleted. \n");
+				break;
+			
+			case '6': break;
+				break;
+			
+			case '7': break;
+				break;
+			
+			case '8': break;
+				break;
+
+			default: 
+				printf("\nPlease enter your choice (1 to 8).\n");
+		}
+		
+		choice = getchar();
+		
+		if (choice != '8'){
+		
+			menu();
+		
+		}
+	}
+
+	return 0;
 
 }
