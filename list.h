579fe63f965f509e68f026496577645ebc751987
alexandrastc <alexandrastc09@gmail.com>
2016-05-13 23:07:29 +0300

    Updated header of new functions

--- list.h
+++ list.h
@@ -1,5 +1,6 @@
 #ifndef _LIST_H_
 #define _LIST_H_ 
+#define MAX_CHAR 1000
 
 typedef struct NOD{
 	
@@ -10,8 +11,15 @@ typedef struct NOD{
 
 } NODE;
 
-	void add_task(NODE *head, int p, char *c, char *t);
+
+	void add_tasks(NODE *head);
 	void print_all(NODE *head);	
+	void print_by_categ(NODE *head);
+	NODE* SortedMerge(NODE* a, NODE* b);
+	void FrontBackSplit(NODE* source, NODE** frontRef, NODE** backRef);
+	void MergeSort(NODE** headRef);
+	void delete_task(NODE *head);
+	void modify_categ(NODE *head);
+	void delete_categ(NODE *head);
 
-
-#endif
+#endif 
